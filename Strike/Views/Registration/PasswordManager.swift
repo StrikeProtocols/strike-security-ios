1309849e8c3b9c3a9777f20c92b38a952028e757
Ata Namvari <ata.namvari@gmail.com>
2022-07-22 11:34:32 -0400

    Adds UI polish

--- Strike/Views/Registration/PasswordManager.swift
+++ Strike/Views/Registration/PasswordManager.swift
@@ -69,11 +69,12 @@ struct PasswordManager: View {
             .multilineTextAlignment(.center)
             .opacity(copied ? 1 : 0)
 
+            Spacer()
+
             NavigationLink {
                 PasswordManagerConfirm(user: user, phrase: phrase, onSuccess: onSuccess)
             } label: {
                 Text("I saved the phrase →")
-                    .frame(height: 100)
                     .frame(maxWidth: .infinity)
             }
             .padding(30)
